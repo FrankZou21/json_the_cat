64d81df5375f85385b65956e890852deef97cec0
Frank <frankzou2000@hotmail.com>
2019-09-26 21:24:16 +0000

    added tests and index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,31 @@
 const request = require("request");
 
-const getInfo = function(webname, callback) {
-  request(webname, (error, response, body) => {
-    callback(body, error);
+const fetchBreedDescription = function(name, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (error, response, body) => {
+    if (JSON.parse(body)[0] === undefined) {
+      error = true;
+      callback(error, null);
+    } else {
+    callback(error, JSON.parse(body)[0].description);
+    }
   })
 }
 
-const printInfo = function(text, err) {
-  if (err) {
-    console.log("There was an error");
-    return;
-  }
-  let output = JSON.parse(text)[0]
-  if (output === undefined) {
-    console.log("Requested breed was not found")
-  } else {
-  console.log(output.description);
-  }
-  return;
-}
+// const printInfo = function(text, err) {
+//   // if (err) {
+//   //   console.log("There was an error");
+//   //   return;
+//   // }
+//   // let output = JSON.parse(text)[0]
+//   // if (output === undefined) {
+//   //   console.log("Requested breed was not found")
+//   // } else {
+//   // console.log(output.description);
+//   // }
+//   // return;
+// }
 
-let input = process.argv.splice(2);
-getInfo(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, printInfo);
+// let input = process.argv.splice(2);
+// fetchBreedDescription(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, printInfo);
+
+module.exports = { fetchBreedDescription };
